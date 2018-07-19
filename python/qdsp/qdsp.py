c96698304d91454970249711dc1ed9e145f51103
Matt Mitchell <mattmitchell2718@gmail.com>
2018-07-19 13:38:06 -0600

    Finished python bindings for full library

--- python/qdsp/qdsp.py
+++ python/qdsp/qdsp.py
@@ -17,6 +17,50 @@ class QDSPplot:
 		# void qdspDelete(QDSPplot *plot);
 		lib.qdspDelete(self.ptr)
 
+	def redraw(self):
+		# void qdspRedraw(QDSPplot *plot);
+		lib.qdspRedraw(self.ptr)
+		
+	def setBGColor(self, rgb):
+		# void qdspSetBGColor(QDSPplot *plot, int rgb);
+		lib.qdspSetBGColor(self.ptr, rgb)
+		
+	def setBounds(self, xmin, xmax, ymin, ymax):
+		# void qdspSetBounds(QDSPplot *plot, double xMin, double xMax,
+		#                    double yMin, double yMax);
+		lib.qdspSetBounds(self.ptr, c_double(xmin), c_double(xmax),
+		                  c_double(ymin), c_double(ymax))
+
+	def setConnected(self, connected):
+		# void qdspSetConnected(QDSPplot *plot, int connected);
+		lib.qdspSetConnected(self.ptr, connected)
+		
+	def setGridX(self, point, interval, rgb):
+		# void qdspSetGridX(QDSPplot *plot, double point, double interval,
+		#                   int rgb);
+		lib.qdspSetGridX(self.ptr, c_double(point), c_double(interval), rgb)
+		
+	def setGridY(self, point, interval, rgb):
+		# void qdspSetGridY(QDSPplot *plot, double point, double interval,
+		#                   int rgb);
+		lib.qdspSetGridY(self.ptr, c_double(point), c_double(interval), rgb)
+		
+	def setFramerate(self, framerate):
+		# void qdspSetFramerate(QDSPplot *plot, double framerate);
+		lib.qdspRedraw(self.ptr, c_double(framerate))
+
+	def setPointAlpha(self, alpha):
+		# void qdspSetPointAlpha(QDSPplot *plot, double alpha);
+		lib.qdspSetPointAlpha(self.ptr, c_double(alpha))
+		
+	def setPointColor(self, rgb):
+		# void qdspSetPointColor(QDSPplot *plot, int rgb);
+		lib.qdspSetPointColor(self.ptr, rgb)
+		
+	def setPointSize(self, pixels):
+		# void qdspSetPointSize(QDSPplot *plot, int pixels);
+		lib.qdspSetPointSize(self.ptr, pixels)
+
 	# helper function for update calls
 	def __xyc2ptr(self, xvals, yvals, colors):
 		# None will not be passed as NULL due to numpy
@@ -52,39 +96,3 @@ class QDSPplot:
 		#                    int *color, int numPoints);
 		xptr, yptr, cptr, size = self.__xyc2ptr(xvals, yvals, colors)
 		return lib.qdspUpdateWait(self.ptr, xptr, yptr, cptr, size)
-	
-	def redraw(self):
-		# void qdspRedraw(QDSPplot *plot);
-		lib.qdspRedraw(self.ptr)
-		
-	def setFramerate(self, framerate):
-		# void qdspSetFramerate(QDSPplot *plot, double framerate);
-		lib.qdspRedraw(self.ptr, c_double(framerate))
-		
-	def setBounds(self, xmin, xmax, ymin, ymax):
-		# void qdspSetBounds(QDSPplot *plot, double xMin, double xMax,
-		#                    double yMin, double yMax);
-		lib.qdspSetBounds(self.ptr, c_double(xmin), c_double(xmax),
-		                  c_double(ymin), c_double(ymax))
-		
-	def setConnected(self, connected):
-		# void qdspSetConnected(QDSPplot *plot, int connected);
-		lib.qdspSetConnected(self.ptr, connected)
-		
-	def setPointColor(self, rgb):
-		# void qdspSetPointColor(QDSPplot *plot, int rgb);
-		lib.qdspSetPointColor(self.ptr, rgb)
-		
-	def setBGColor(self, rgb):
-		# void qdspSetBGColor(QDSPplot *plot, int rgb);
-		lib.qdspSetBGColor(self.ptr, rgb)
-		
-	def setGridX(self, point, interval, rgb):
-		# void qdspSetGridX(QDSPplot *plot, double point, double interval,
-		#                   int rgb);
-		lib.qdspSetGridX(self.ptr, c_double(point), c_double(interval), rgb)
-		
-	def setGridY(self, point, interval, rgb):
-		# void qdspSetGridY(QDSPplot *plot, double point, double interval,
-		#                   int rgb);
-		lib.qdspSetGridY(self.ptr, c_double(point), c_double(interval), rgb)
